fa806a53ea9e155b027ae604f6780553fcee27c6
Lin Onetwo <linonetwo012@gmail.com>
2021-10-23 17:41:45 +0800

    feat: add store for value logics

--- src/store/models/index.ts
+++ src/store/models/index.ts
@@ -2,10 +2,12 @@ import { Models } from '@rematch/core';
 
 import { uiState } from './uiState';
 import { bookState } from './bookState';
+import { valueState } from './valueState';
 
 export interface RootModel extends Models<RootModel> {
   bookState: typeof bookState;
   uiState: typeof uiState;
+  valueState: typeof valueState;
 }
 
-export const models: RootModel = { uiState, bookState };
+export const models: RootModel = { uiState, bookState, valueState };
