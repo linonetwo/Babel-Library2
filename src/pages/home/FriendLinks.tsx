8797a098351967cbcff4c091cce1ed3d1600e8b8
Lin Onetwo <linonetwo012@gmail.com>
2021-10-23 18:25:39 +0800

    Update FriendLinks.tsx

--- src/pages/home/FriendLinks.tsx
+++ src/pages/home/FriendLinks.tsx
@@ -32,20 +32,41 @@ export function FriendLinks(): JSX.Element {
     <Container>
       <Title>感谢</Title>
       <FriendLinkRow>
-        开发组：<a href="https://github.com/linonetwo">林一二</a> <a href="">李子申</a> <a href="https://github.com/Here21">张灏哲</a>
+        开发组：
+        <a target="_blank" href="https://github.com/linonetwo" rel="noreferrer">
+          林一二
+        </a>
+        <a target="_blank" href="">
+          李子申
+        </a>
+        <a target="_blank" href="https://github.com/Here21" rel="noreferrer">
+          张灏哲
+        </a>
       </FriendLinkRow>
       <span>项目核心引擎：基于模板的「基于模板的文本自动生成器」自动生成器！（在本次Hackathon内开发！）</span>
       <FriendLinkRow>
-        <a href="https://github.com/linonetwo/template-based-generator-template">Github Template</a>
-        <a href="https://onetwo.ren/template-based-generator-template/">在线试玩</a>
+        <a target="_blank" href="https://github.com/linonetwo/template-based-generator-template" rel="noreferrer">
+          Github Template
+        </a>
+        <a target="_blank" href="https://onetwo.ren/template-based-generator-template/" rel="noreferrer">
+          在线试玩
+        </a>
       </FriendLinkRow>
 
       <span>我们使用了基于 CC BY-SA 3.0 协议的 CDDA 游戏知识图谱！在此希望更多人入坑！</span>
       <FriendLinkRow>
-        <a href="cataclysmdda.org/">官网</a>
-        <a href="https://github.com/CleverRaven/Cataclysm-DDA/releases/latest">官方Github</a>
-        <a href="https://cdda.fun/">中文社区</a>
-        <a href="https://github.com/linonetwo/CDDA-Kenan-Modpack-Chinese">社区图谱包</a>
+        <a target="_blank" href="cataclysmdda.org/">
+          官网
+        </a>
+        <a target="_blank" href="https://github.com/CleverRaven/Cataclysm-DDA/releases/latest" rel="noreferrer">
+          官方Github
+        </a>
+        <a target="_blank" href="https://cdda.fun/" rel="noreferrer">
+          中文社区
+        </a>
+        <a target="_blank" href="https://github.com/linonetwo/CDDA-Kenan-Modpack-Chinese" rel="noreferrer">
+          社区图谱包
+        </a>
       </FriendLinkRow>
     </Container>
   );
