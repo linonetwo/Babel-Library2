6b1b929577dc9fcce0aacc2b9e32069f6e262e6d
Lin Onetwo <linonetwo012@gmail.com>
2021-10-23 17:05:33 +0800

    docs: 强调自动生成器在本次Hackathon内开发

--- src/pages/home/FriendLinks.tsx
+++ src/pages/home/FriendLinks.tsx
@@ -34,16 +34,18 @@ export function FriendLinks(): JSX.Element {
       <FriendLinkRow>
         开发组：<a href="https://github.com/linonetwo">林一二</a> <a href="">李子申</a> <a href="https://github.com/Here21">张灏哲</a>
       </FriendLinkRow>
-      <span>项目核心引擎：基于模板的「基于模板的文本自动生成器」自动生成器！</span>
+      <span>项目核心引擎：基于模板的「基于模板的文本自动生成器」自动生成器！（在本次Hackathon内开发！）</span>
       <FriendLinkRow>
         <a href="https://github.com/linonetwo/template-based-generator-template">Github Template</a>
         <a href="https://onetwo.ren/template-based-generator-template/">在线试玩</a>
       </FriendLinkRow>
 
-      <span>我们使用了基于 CC BY-SA 3.0 协议的 CDDA 游戏知识图谱！</span>
+      <span>我们使用了基于 CC BY-SA 3.0 协议的 CDDA 游戏知识图谱！在此希望更多人入坑！</span>
       <FriendLinkRow>
-        <a href="cataclysmdda.org/">Official Website</a>
+        <a href="cataclysmdda.org/">官网</a>
+        <a href="https://github.com/CleverRaven/Cataclysm-DDA/releases/latest">官方Github</a>
         <a href="https://cdda.fun/">中文社区</a>
+        <a href="https://github.com/linonetwo/CDDA-Kenan-Modpack-Chinese">社区图谱包</a>
       </FriendLinkRow>
     </Container>
   );
